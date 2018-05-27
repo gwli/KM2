79dfe66364f5a8ee52abd12af413611f0be04427
Victor Li <gwli@outlook.com>
2018-05-28 00:39:53 +0800

    add authentication

--- mongo_docker/docker_cmd.sh
+++ mongo_docker/docker_cmd.sh
@@ -1,2 +1,6 @@
+sudo rm -fr mongo_db
 mkdir mongo_db
-docker run --name mongo   -v `pwd`/mongo_db:/data/db -p 27017:27017 -d  mongo --dbpath /data/db
+#docker run --name mongo   -v `pwd`/mongo_db:/data/db -p 27017:27017 -d  mongo --dbpath /data/db
+#docker run --name mongo -v /my/custom:/etc/mongo -d mongo --config /etc/mongo/mongod.conf
+docker rm -f mongodb
+docker run -d --name mongodb  -v `pwd`/mongo_db:/data/db -p 27017:27017  -e MONGO_INITDB_ROOT_USERNAME=mongoadmin -e MONGO_INITDB_ROOT_PASSWORD=root mongo --dbpath /data/db
