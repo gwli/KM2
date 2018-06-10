9d15793b8d5f958602868e7240f3eca8042f9b5a
Victor Li <gwli@outlook.com>
2018-06-10 18:35:06 +0800

    trial on the media editor

--- DailyEvolve/daily/src/components/dailytodo/Dailytodo.vue
+++ DailyEvolve/daily/src/components/dailytodo/Dailytodo.vue
@@ -2,13 +2,22 @@
    <div>
     <ul v-if="posts && posts.length">
       <li v-for="post of posts">
-        <p><strong>{{ post.tid }}  {{post.title}}</strong></p>
-        <p>description: {{post.description}}</p>
-        <p>status: {{post.status}}</p>
-        <p>labels: {{post.tags}}</p>
-        <p>Date: {{post.due_date}}</p>
-        <p>weight: {{post.Weight}}</p>
-      
+          <div class="medium-editor">
+            <div class="row">
+              <div class="col-md-12">
+                <vuestic-widget :headerText="post.title">
+                  <vuestic-medium-editor @initialized="handleEditorInitialization" :editor-options="editorOptions">
+                    <blockquote class="blockquote">
+                    <p>description: {{post.description}}</p>
+                    </blockquote>
+                    <hr/>
+                    <b class="default-selection">status</b>{{post.status}}  labels: {{post.tags}} Date: {{post.due_date}}
+                  </vuestic-medium-editor>
+                    <div> Weight: {{post.Weight}} <button>vote</button></div>
+                </vuestic-widget>
+              </div>
+            </div>
+          </div>
       </li>
     </ul>
 
@@ -21,16 +30,90 @@
 </template>
 
 <script>
+  export default {
+    name: 'medium-editor',
+
+    data () {
+      return {
+        editor: {},
+        editorOptions: {
+          buttonLabels: 'fontawesome',
+          autoLink: true,
+          toolbar: {
+            buttons: [
+              'bold',
+              'italic',
+              'underline',
+              'anchor',
+              'h1',
+              'h2',
+              'h3'
+            ]
+          }
+        }
+      }
+    },
+
+    methods: {
+      handleEditorInitialization (editor) {
+        this.editor = editor
+        this.$nextTick(() => {
+          this.highlightSampleText()
+        })
+      },
+
+      highlightSampleText () {
+        let sampleText = document.getElementsByClassName('default-selection')[0]
+        this.editor.selectElement(sampleText)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
+
+<script>
   import axios from 'axios'
   
   export default {
     data () {
       return {
         posts: [],
-        errors_message: []
+        errors_message: [],
+        editor: {},
+        editorOptions: {
+          buttonLabels: 'fontawesome',
+          autoLink: true,
+          toolbar: {
+            buttons: [
+              'bold',
+              'italic',
+              'underline',
+              'anchor',
+              'h1',
+              'h2',
+              'h3'
+            ]
+          }
+        }
       }
     },
   
+    methods: {
+      handleEditorInitialization (editor) {
+        this.editor = editor
+        this.$nextTick(() => {
+          this.highlightSampleText()
+        })
+      },
+
+      highlightSampleText () {
+        let sampleText = document.getElementsByClassName('default-selection')[0]
+        this.editor.selectElement(sampleText)
+      }
+    },
     // Fetches posts when the component is created.
     created () {
       axios.get(`http://10.19.226.116:3030/tickets`)
@@ -46,3 +129,6 @@
     }
   }
 </script>
+<style lang="scss">
+
+</style>
